ddae2a464880cd09252746df26cfab5f83431ea5
Paulo Matos <p@ocmatos.com>
2023-04-25 18:22:14 +0200

    [runtime] Add support for integer subtraction

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -28,11 +28,53 @@ public:
   virtual void accept(ASTVisitor &V) const override { V.visit(*this); }
 };
 
+class SubtractFunction : public ast::RuntimeFunction {
+public:
+  SubtractFunction(const std::wstring &Name) : RuntimeFunction(Name) {}
+
+  virtual std::unique_ptr<ast::ValueNode>
+  operator()(const std::vector<const ast::ValueNode *> &Args) const override {
+    if (Args.empty()) {
+      // Runtime error, no args to -
+      // FIXME: issue error message
+      return nullptr;
+    }
+
+    std::unique_ptr<ast::Integer> Sub;
+    bool First = true;
+
+    for (const auto *Arg : Args) {
+      if (auto const *I = llvm::dyn_cast<ast::Integer>(Arg)) {
+        if (First) {
+
+          Sub = std::unique_ptr<ast::Integer>(
+              llvm::cast<ast::Integer>(I->clone()));
+          First = false;
+        } else {
+          *Sub -= *I;
+        }
+      } else {
+        // FIXME: Throw an error. Unsupported type for -.
+        return nullptr;
+      }
+    }
+
+    return Sub;
+  }
+
+  virtual ast::RuntimeFunction *clone() const override {
+    return new SubtractFunction(*this);
+  }
+
+  virtual void accept(ASTVisitor &V) const override { V.visit(*this); }
+};
+
 #define RUNTIME_FUNC(Identifier, Name)                                         \
   RuntimeFunctions[Identifier] = std::make_shared<Name>(Identifier);
 Runtime::Runtime() {
   // List of runtime functions.
   RUNTIME_FUNC(L"+", AddFunction);
+  RUNTIME_FUNC(L"-", SubtractFunction);
 }
 
 std::unique_ptr<ast::ValueNode>
